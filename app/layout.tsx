5f73e7902c491d486881a3f243742a9ccf34aeb1
BABA Marcel <babamarcel80@gmail.com>
2025-12-02 18:03:13 +0000

    Updated layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,10 +14,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="fr">
+
 <head>
 <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8845765535417046"
      crossorigin="anonymous"></script>
 </head>
+
       <body
         className={` antialiased`}
       >
